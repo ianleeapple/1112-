67d2edab6610b009f678083f2fe6bda4a44417d4
ianleeapple <126649300+ianleeapple@users.noreply.github.com>
2023-06-02 21:14:22 +0800

    Update Sudoku.h

--- Sudoku.h
+++ Sudoku.h
@@ -1,1 +1,22 @@
+#include<iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+#ifndef SUDOKU_H
+#define SUDOKU_H
+class Sudoku
+{
+private:
+    vector<vector<int>> board;
+public:
+    Sudoku();
+    void generate();
+    void print();
+    bool isValid(int row, int col, int num);
+    bool findEmptyCell(int& row, int& col);
+    bool solve();
+    void play();
+};
+#endif
