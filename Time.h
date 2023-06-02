b9395688060462d153837b141bbf51241fb8bda6
ianleeapple <126649300+ianleeapple@users.noreply.github.com>
2023-06-02 21:15:06 +0800

    Update Time.h

--- Time.h
+++ Time.h
@@ -1,1 +1,24 @@
+#include<iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+#ifndef TIME_H
+#define TIME_H
+class Time
+{
+private:
+    string name;
+    double timeTaken;
+    clock_t startTime;
+public:
+    Time(string a);
+    void setname(string a);
+    string getname();
+    void start();
+    void stop();
+    double getTimeTaken();
+    void printTimeTaken();
+};
+#endif
