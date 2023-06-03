35b54a9e801070e6d00a1e536d1e135bd73b0886
ianleeapple <126649300+ianleeapple@users.noreply.github.com>
2023-06-03 23:29:45 +0800

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "Time.h"
+using namespace std;
 
+Time::Time(string a)
+{
+    name = a;
+    timeTaken = 0.0;
+}
+void Time::setname(string a)
+{
+    name = a;
+}
+string Time::getname()
+{
+    return name;
+}
+void Time::start()
+{
+    startTime = clock();
+}
+void Time::stop()
+{
+    clock_t endTime = clock();
+    timeTaken = double(endTime - startTime) / CLOCKS_PER_SEC;
+}
+double Time::getTimeTaken()
+{
+    return timeTaken;
+}
+void Time::printTimeTaken()
+{
+    cout << "執行時間:" << timeTaken << "秒" << endl;
+}
